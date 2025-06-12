ce009fc770bb44e1e1b5bb6550b2a1e5971c2ecd
Narkkarux Tripiyaratana <narkkharak12345@hotmail.com>
2025-06-12 14:03:21 +0700

    Try to fix strapi component disappear issue

--- cms-strapi/config/database.ts
+++ cms-strapi/config/database.ts
@@ -1,33 +1,60 @@
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT");
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "bank"),
+//       username: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "0000"),
+//       schema: env("DATABASE_SCHEMA", "public"),
+//     },
+//   }
+// });
 
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
+const path = require('path');
 
-      pool: {
-        min: env.int("DATABASE_POOL_MIN"),
-        max: env.int("DATABASE_POOL_MAX"),
-      },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client,
-
-      ...connections[client],
-
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
+
+// module.exports = ({ env }) => {
+//   const client = env("DATABASE_CLIENT");
+
+//   const connections = {
+//     postgres: {
+//       connection: {
+//         connectionString: env("DATABASE_URL"),
+//         ssl: env.bool("DATABASE_SSL", false) && {
+//           rejectUnauthorized: env.bool(
+//             "DATABASE_SSL_REJECT_UNAUTHORIZED",
+//             true
+//           ),
+//         },
+//         schema: env("DATABASE_SCHEMA", "public"),
+//       },
+
+//       pool: {
+//         min: env.int("DATABASE_POOL_MIN"),
+//         max: env.int("DATABASE_POOL_MAX"),
+//       },
+//     },
+//   };
+
+//   return {
+//     connection: {
+//       client,
+
+//       ...connections[client],
+
+//       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//     },
+//   };
+// };
--- cms-strapi/types/generated/components.d.ts
+++ cms-strapi/types/generated/components.d.ts
@@ -62,6 +62,17 @@ export interface SharedSlider extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedTest extends Struct.ComponentSchema {
+  collectionName: 'components_shared_tests';
+  info: {
+    displayName: 'test';
+    icon: 'chartPie';
+  };
+  attributes: {
+    testenum: Schema.Attribute.Enumeration<['me', 'you']>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
@@ -70,6 +81,7 @@ declare module '@strapi/strapi' {
       'shared.rich-text': SharedRichText;
       'shared.seo': SharedSeo;
       'shared.slider': SharedSlider;
+      'shared.test': SharedTest;
     }
   }
 }
